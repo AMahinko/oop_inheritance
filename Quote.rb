d4718e7b8b90a648601c4b0b22edf1ea076017c2
Alex <alex.mahinko@gmail.com>
2017-04-03 15:45:54 -0400

    Quote genius created, and quotes list populated.

---
+++ Quote.rb
@@ -0,0 +1,24 @@
+require "./MathGenius"
+
+@@quote_store = [
+  "The saddest aspect of life right now is that science gathers knowledge
+  faster than society gathers wisdom.",
+
+  "Never memorize something that you can look up.",
+
+  "Science without religion is lame, religion without science is blind.",
+
+  "If we knew what it was we were doing, it would not be called research, would it?",
+
+  "One, remember to look up at the stars and not down at your feet.
+  Two, never give up work. Work gives you meaning and purpose and life is empty without it.
+  Three, if you are lucky enough to find love, remember it is there and don't throw it away."
+]
+
+class Quote
+  super
+
+  
+
+
+end
