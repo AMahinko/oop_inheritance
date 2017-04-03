45f9948404e2422a1dba806d46adf480067a41f4
Alex <alex.mahinko@gmail.com>
2017-04-03 14:41:24 -0400

    defined local language and travel functions, as well as implemented (untested)function to translate the result of addition.

--- MathGenius.rb
+++ MathGenius.rb
@@ -2,16 +2,29 @@ require "./multilinguist.rb"
 
 class MathGenius
 
-  def initialize
+  def initialize(num_array)
     super
+    @num_array = num_array
   end
 
-  def self.add(num_array)
+  def add
     total = 0
-    num_array.each do |number|
+    @num_array.each do |number|
       total += number
     end
     return total
+  end
+
+  def travel_to
+    super
+  end
+
+  def say_in_local_language
+    super
+  end
+
+  def multilang_add
+    say_in_local_language("Your sum is #{@num_array.add}")
   end
 
 
