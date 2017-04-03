7461904263cbd762c2530ee90ac0523f2fe3368d
Alex <alex.mahinko@gmail.com>
2017-04-03 14:35:34 -0400

    MathGenius initialized and addition function now works

--- MathGenius.rb
+++ MathGenius.rb
@@ -1,8 +1,19 @@
-require "multilinguist.rb"
+require "./multilinguist.rb"
 
 class MathGenius
 
-  
+  def initialize
+    super
+  end
+
+  def self.add(num_array)
+    total = 0
+    num_array.each do |number|
+      total += number
+    end
+    return total
+  end
+
 
 
 end
